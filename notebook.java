32e7b4fae2026d58794619e550e91e63e5bc6868
GrinevaNataly <162015539+NatalyGit@users.noreply.github.com>
2024-11-05 12:35:50 +0300

    Update notebook.java

--- notebook.java
+++ notebook.java
@@ -12,23 +12,19 @@ public class Notebook {
     }
     public List<Note> getNotesForDay(LocalDateTime dateTime) {
         return notes.stream()
-                .filter(note ->
-                        note.getDateTime().toLocalDate().isEqual(dateTime.toLocalDate()))
+                .filter(note -> note.getDateTime().toLocalDate().isEqual(dateTime.toLocalDate()))
                 .sorted(Comparator.comparing(Note::getDateTime))
                 .collect(Collectors.toList());
     }
     public List<Note> getNotesForWeek(LocalDateTime startOfWeek) {
         LocalDateTime endOfWeek = startOfWeek.plusWeeks(1);
         return notes.stream()
-                .filter(note ->
-                        !note.getDateTime().isBefore(startOfWeek) &&
-                                !note.getDateTime().isAfter(endOfWeek))
+                .filter(note -> !note.getDateTime().isBefore(startOfWeek) && !note.getDateTime().isAfter(endOfWeek))
                 .sorted(Comparator.comparing(Note::getDateTime))
                 .collect(Collectors.toList());
     }
     public void saveToFile(String fileName) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new
-                FileWriter(fileName))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             for (Note note : notes) {
                 writer.write(note.toString());
                 writer.newLine();
@@ -37,13 +33,11 @@ public class Notebook {
     }
     public void loadFromFile(String fileName) throws IOException {
         notes.clear();
-        try (BufferedReader reader = new BufferedReader(new
-                FileReader(fileName))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(": ", 2);
-                LocalDateTime dateTime =
-                        LocalDateTime.parse(parts[0]);
+                LocalDateTime dateTime = LocalDateTime.parse(parts[0]);
                 String description = parts[1];
                 notes.add(new Note(dateTime, description));
             }
