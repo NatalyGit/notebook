99501e11b5b3d7ad5f434579a1e10e416dee8db3
GrinevaNataly <162015539+NatalyGit@users.noreply.github.com>
2024-11-05 12:32:32 +0300

    Update main.java

--- main.java
+++ main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Notebook model = new Notebook();
         NotebookView view = new ConsoleNotebookView();
-        NotebookPresenter presenter = new NotebookPresenter(model,
-                view);
+        NotebookPresenter presenter = new NotebookPresenter(model, view);
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("1. Add Note");
@@ -16,7 +15,7 @@ public class Main {
             System.out.println("5. Load Notes");
             System.out.println("6. Exit");
             int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
+            scanner.nextLine(); 
             switch (choice) {
                 case 1:
                     presenter.addNote();
